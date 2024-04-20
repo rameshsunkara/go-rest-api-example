bcf45255f3dc566db84951a3074ff5a3134ee79f
Ramesh Sunkara <ramesh.sunkara@hpe.com>
2024-04-20 04:51:00 -0400

    add more tests

--- internal/middleware/internalAuth.go
+++ internal/middleware/internalAuth.go
@@ -4,8 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func InternalAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// TODO: Generally we would valid JWT token
-		// TODO: Check for additional permissions for internal apis
 		c.Next()
 	}
 }
