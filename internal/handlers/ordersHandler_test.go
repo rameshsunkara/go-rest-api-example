ad449ede0df38b2d6c05bb8342e0e202fed7fe48
Ramesh Sunkara <ramesh.sunkara@hpe.com>
2024-04-27 19:03:12 -0400

    added more tests

--- internal/handlers/ordersHandler_test.go
+++ internal/handlers/ordersHandler_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func UnMarshalOrderResponse(d []byte) (*data.Order, error) {
+func UnMarshalOrderData(d []byte) (*data.Order, error) {
 	var r data.Order
 	err := json.Unmarshal(d, &r)
 	if err != nil {
@@ -43,9 +43,10 @@ func UnMarshalOrdersData(d []byte) (*[]data.Order, error) {
 }
 
 func TestOrdersHandler_Create_Success(t *testing.T) {
-	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
-	r := gin.New()
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
 		CreateFunc: func(_ context.Context, _ *data.Order) (string, error) {
 			return "1", nil
@@ -60,17 +61,13 @@ func TestOrdersHandler_Create_Success(t *testing.T) {
 	}
 
 	body, _ := json.Marshal(orderInput)
-	req, err := http.NewRequest(http.MethodPost, "/orders", bytes.NewReader(body))
-	require.NoError(t, err)
+	c.Request, _ = http.NewRequest(http.MethodPost, "/orders", bytes.NewReader(body))
+	r.ServeHTTP(recorder, c.Request)
 
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.Equal(t, http.StatusCreated, recorder.Code)
 
 	var responseOrder external.Order
-	err = json.Unmarshal(w.Body.Bytes(), &responseOrder)
+	err := json.Unmarshal(recorder.Body.Bytes(), &responseOrder)
 	require.NoError(t, err)
 
 	assert.Equal(t, int64(1), responseOrder.Version)
@@ -85,75 +82,63 @@ func TestOrdersHandler_Create_Success(t *testing.T) {
 }
 
 func TestOrdersHandler_Create_InvalidInput(t *testing.T) {
-	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
-	r := gin.New()
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
 		CreateFunc: func(_ context.Context, _ *data.Order) (string, error) {
 			return "mock_order_id", nil
 		},
 	}, lgr)
 	r.POST("/orders", handler.Create)
-
 	invalidInput := "{ invalid JSON }"
-	req, err := http.NewRequest(http.MethodPost, "/orders", bytes.NewReader([]byte(invalidInput)))
-	require.NoError(t, err)
+	c.Request, _ = http.NewRequest(http.MethodPost, "/orders", bytes.NewReader([]byte(invalidInput)))
 
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+	r.ServeHTTP(recorder, c.Request)
 
 	var apiErr external.APIError
-	err = json.Unmarshal(w.Body.Bytes(), &apiErr)
+	err := json.Unmarshal(recorder.Body.Bytes(), &apiErr)
 	require.NoError(t, err)
-
 	assert.Equal(t, http.StatusBadRequest, apiErr.HTTPStatusCode)
 	assert.Equal(t, "orders_create_invalid_input", apiErr.ErrorCode)
 	assert.Equal(t, "Invalid order request body", apiErr.Message)
 }
 
 func TestOrdersHandler_Create_InternalServerError(t *testing.T) {
-	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
-	r := gin.New()
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
 		CreateFunc: func(_ context.Context, _ *data.Order) (string, error) {
 			return "", errors.New(errors2.UnexpectedErrorMessage)
 		},
 	}, lgr)
 	r.POST("/orders", handler.Create)
-
 	orderInput := external.OrderInput{
 		Products: []external.ProductInput{
 			{Name: "Product 1", Price: 10.0, Quantity: 2},
 		},
 	}
-
 	body, _ := json.Marshal(orderInput)
-	req, _ := http.NewRequest(http.MethodPost, "/orders", bytes.NewReader(body))
+	c.Request, _ = http.NewRequest(http.MethodPost, "/orders", bytes.NewReader(body))
 
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+	r.ServeHTTP(recorder, c.Request)
 
-	assert.Equal(t, http.StatusInternalServerError, w.Code)
-
+	assert.Equal(t, http.StatusInternalServerError, recorder.Code)
 	var apiErr external.APIError
-	err := json.Unmarshal(w.Body.Bytes(), &apiErr)
+	err := json.Unmarshal(recorder.Body.Bytes(), &apiErr)
 	require.NoError(t, err)
-
 	assert.Equal(t, http.StatusInternalServerError, apiErr.HTTPStatusCode)
-	assert.Equal(t, "orders_create_server_error", apiErr.ErrorCode)
-	assert.Equal(t, errors2.UnexpectedErrorMessage, apiErr.Message) // Assuming errors.UnexpectedErrorMessage
+	assert.Equal(t, errors2.UnexpectedErrorMessage, apiErr.Message)
 }
 
 func TestGetAllOrdersSuccess(t *testing.T) {
-	// Test Setup
-	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
-	r := gin.New()
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
 		GetAllFunc: func(_ context.Context, _ int64) (*[]data.Order, error) {
 			dataBytes, err := os.ReadFile("../mockData/orders.json")
@@ -165,27 +150,23 @@ func TestGetAllOrdersSuccess(t *testing.T) {
 		},
 	}, lgr)
 	r.GET("/orders", handler.GetAll)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/orders", nil)
 
-	req, err := http.NewRequest(http.MethodGet, "/orders", nil)
-	require.NoError(t, err)
+	r.ServeHTTP(recorder, c.Request)
 
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-
+	assert.Equal(t, http.StatusOK, recorder.Code)
 	var respOrders []external.Order
-	err = json.Unmarshal(w.Body.Bytes(), &respOrders)
+	err := json.Unmarshal(recorder.Body.Bytes(), &respOrders)
 	require.NoError(t, err)
 	assert.Len(t, respOrders, 10)
 }
 
 func TestGetAllOrdersFailure_DBRead(t *testing.T) {
 	// Test Setup
-	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
-	r := gin.New()
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
 		GetAllFunc: func(_ context.Context, _ int64) (*[]data.Order, error) {
 			dataBytes, err := os.ReadFile("../mockData/non-existent.json")
@@ -197,57 +178,76 @@ func TestGetAllOrdersFailure_DBRead(t *testing.T) {
 		},
 	}, lgr)
 	r.GET("/orders", handler.GetAll)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/orders", nil)
 
-	req, err := http.NewRequest(http.MethodGet, "/orders", nil)
-	require.NoError(t, err)
+	r.ServeHTTP(recorder, c.Request)
 
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusInternalServerError, w.Code)
+	assert.Equal(t, http.StatusInternalServerError, recorder.Code)
 }
 
 func TestGetAllOrdersFailure_LimitOutOfBounds(t *testing.T) {
 	// Test Setup
-	resp := httptest.NewRecorder()
+	recorder := httptest.NewRecorder()
 	gin.SetMode(gin.TestMode)
-	c, r := gin.CreateTestContext(resp)
-	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{}, lgr)
 	r.GET("/orders", handler.GetAll)
-
 	c.Request, _ = http.NewRequest(http.MethodGet, "/orders", nil)
 	q := c.Request.URL.Query()
 	q.Add("limit", "10000")
 	c.Request.URL.RawQuery = q.Encode()
 
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, c.Request)
+	r.ServeHTTP(recorder, c.Request)
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
 }
 
 func TestGetAllOrdersFailure_InvalidLimit(t *testing.T) {
 	// Test Setup
-	resp := httptest.NewRecorder()
+	recorder := httptest.NewRecorder()
 	gin.SetMode(gin.TestMode)
-	c, r := gin.CreateTestContext(resp)
+	c, r := gin.CreateTestContext(recorder)
 	gin.SetMode(gin.TestMode)
 	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
 	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{}, lgr)
 	r.GET("/orders", handler.GetAll)
-
 	c.Request, _ = http.NewRequest(http.MethodGet, "/orders", nil)
 	q := c.Request.URL.Query()
 	q.Add("limit", "ABC")
 	c.Request.URL.RawQuery = q.Encode()
 
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, c.Request)
+	r.ServeHTTP(recorder, c.Request)
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+}
+
+func TestGetOrderByIDSuccess(t *testing.T) {
+	// Test Setup
+	lgr := logger.Setup(models.ServiceEnv{Name: "test"})
+	recorder := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, r := gin.CreateTestContext(recorder)
+	handler := handlers.NewOrdersHandler(&mocks.MockOrdersDataService{
+		GetByIDFunc: func(_ context.Context, _ string) (*data.Order, error) {
+			dataBytes, err := os.ReadFile("../mockData/order.json")
+			if err != nil {
+				return nil, err
+			}
+			dataOrder, _ := UnMarshalOrderData(dataBytes)
+			return dataOrder, nil
+		},
+	}, lgr)
+	r.GET("/orders/609d9ed771df2a0d99bf0077", handler.GetByID)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/orders/609d9ed771df2a0d99bf0077", nil)
+
+	r.ServeHTTP(recorder, c.Request)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	var respOrder external.Order
+	err := json.Unmarshal(recorder.Body.Bytes(), &respOrder)
+	require.NoError(t, err)
+	assert.Equal(t, "609d9ed771df2a0d99bf0077", respOrder.ID)
 }
 
 /*
